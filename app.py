0089b3516037ca3332247764edc466ab38c339b1
killkah <152780687+killkah@users.noreply.github.com>
2025-05-08 20:54:36 +0700

    Summery

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+
+@app.route('/spo')
+def spo():
+    return render_template('spo.html')
+
 app.run(debug=True)
